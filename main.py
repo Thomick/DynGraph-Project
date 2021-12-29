fc75f8311f9ba4e92e131e1336e6efaa0d8af578
Thomas MICHEL <tmdu13@gmail.com>
2021-12-29 22:21:42 +0100

    Ex 4 and 5

--- main.py
+++ main.py
@@ -28,3 +28,16 @@ if __name__ == "__main__":
     convert2splitted("data/Infocom06")
     print("    Converting RollerNet -> RollerNet_splitted")
     convert2splitted("data/RollerNet")
+
+    ######### EX 4  -> see get_intercontact_duration ########
+
+    ######### EX 5 ###############
+    print("Exercice 5 :")
+    print("    Plotting intercontact duration distributon for Infocom06 ...")
+    contacts = parse_raw_data("data/Infocom06")
+    inter_dur = get_intercontact_duration(contacts)
+    plot_distrib(inter_dur)
+    print("    Plotting intercontact duration distributon for RollerNet ...")
+    contacts = parse_raw_data("data/RollerNet")
+    inter_dur = get_intercontact_duration(contacts)
+    plot_distrib(inter_dur)
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def parse_data(filename):
+def parse_raw_data(filename):
     contacts = []
     with open(filename, 'r') as f:
         for line in f.readlines():
@@ -10,8 +11,18 @@ def parse_data(filename):
     return np.array(contacts, dtype=int)
 
 
+def parse_splitted_data(filename):
+    contacts = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            n1, n2, ts, te = line.split(sep=" ")
+            te = 0 if te == "C" else 1
+            contacts.append([int(n1), int(n2), int(ts), int(te)])
+    return np.array(contacts, dtype=int)
+
+
 def datainfo(filename):
-    contacts = parse_data(filename)
+    contacts = parse_raw_data(filename)
     nb_node = len(set.union(set(contacts[:, 0]), set(contacts[:, 1])))
     nb_contact = contacts.shape[0]
     duration = np.max(contacts[:, 3]) - np.min(contacts[:, 3])
@@ -19,14 +30,14 @@ def datainfo(filename):
 
 
 def format_file(filename):
-    contacts = parse_data(filename)
+    contacts = parse_raw_data(filename)
     delta = np.min(contacts[:, 3])
     contacts[:, (2, 3)] -= delta
     np.savetxt(filename+"_formatted", contacts, fmt="%s")
 
 
 def convert2splitted(filename):
-    contacts = parse_data(filename)
+    contacts = parse_raw_data(filename)
     zeros = np.zeros((contacts.shape[0], 1), dtype=int)
     creation = np.concatenate((contacts[:, (2, 0, 1)], zeros), axis=1)
     suppression = np.concatenate((contacts[:, (3, 0, 1)], zeros+1), axis=1)
@@ -37,3 +48,16 @@ def convert2splitted(filename):
         for i in range(merged.shape[0]):
             f.write(
                 f"{merged[i,0]} {merged[i,1]} {merged[i,2]} {'C' if merged[i,3]==0 else 'S'}\n")
+
+
+def get_intercontact_duration(contacts):
+    inter_duration = []
+    for i in range(contacts.shape[0]-1):
+        if contacts[i+1, 0] == contacts[i, 0] and contacts[i+1, 1] == contacts[i, 1]:
+            inter_duration.append(contacts[i+1, 2]-contacts[i, 3]-1)
+    return inter_duration
+
+
+def plot_distrib(data):
+    plt.hist(data,200)
+    plt.show()
